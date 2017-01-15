183dc7bbab4b39d03361ff77f27d4d15d8d4797e
Christina L. Cole <christinalcole@gmail.com>
2017-01-15 16:19:09 -0500

    initial migration, models

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
-class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+class CreateLandmarks < ActiveRecord::Migration #file provided by Flatiron at start of lab
+  #raise "Write CreateLandmarks migration here"
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.datetime :year_completed
+    end
+  end
 end
